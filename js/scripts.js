4fda2921d8718786b9537250cd7b0fbfbc8b0653
BornToDoStuff <borntodostuff@gmail.com>
2017-09-22 16:36:16 -0400

    combining scripts

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+---
+---
+//JQuery-3.1.1.min.js
+{% include /js/jquery-3.1.1.min.js %}
+//custom-bootstrap.js
+{% include /js/custom-bootstrap.min.js %}
+//lunr.min.js
+{% include /js/lunr.min.js %}
+//mustache.min.js
+{% include /js/mustache.min.js %}
