87974ff627eee4f1ae3b719965efac87846ae4b2
BornToDoStuff <borntodostuff@gmail.com>
2017-09-22 13:58:19 -0400

    implement the beginnings of search

---
+++ js/search-index.js
@@ -0,0 +1,55 @@
+---
+---
+
+var search_index = lunr(function () {
+  this.ref("id");
+  this.field("title", {boost: 10});
+
+  this.field('category');
+  this.field("tags");
+  this.field("searchtext");
+
+});
+
+{% for post in site.posts %}
+search_index.add({
+  "id" : "{{ post.item_id }}",
+  "title" : "{{ post.title | escape }}",
+  "type" : "{{ post.item_type }}",
+  "subtypes" : "{% for item in post.item_subtype %}{{ item.type }}{% unless forloop.last %} {% endunless %}{% endfor %}",
+  "rarity" : "{{ post.item_rarity }}",
+  "attunement" : {{ post.item_attunement }},
+  "requirement" : "{{ post.item_requirement }}",
+  "curse" : {{ post.item_type }},
+  "classes" : "{% for item in post.item_classes %}{{ item.class }}{% unless forloop.last %} {% endunless %}{% endfor %}",
+  "school" : "{{ post.item_school }}",
+  "role" : "{% for item in post.item_role %}{{ item.role }}{% unless forloop.last %} {% endunless %}{% endfor %}",
+  "damage" : "{% for item in post.item_damage %}{{ item.type }}{% unless forloop.last %} {% endunless %}{% endfor %}",
+  "tags" : "{% for item in post.item_tags %}{{ item.tag }}{% unless forloop.last %} {% endunless %}{% endfor %}",
+  "searchtext" : {{ post.content | strip_html | strip_newlines | jsonify }}
+});
+{% endfor %}
+
+var search_cache = [
+  {% for post in site.posts %}
+    {
+      "id" : "{{ post.item_id }}",
+      "title" : "{{ post.title | escape }}",
+      "type" : "{{ post.item_type }}",
+      "subtypes" : [ {% for item in post.item_subtype %} {"type" : "{{ item.type }}"}{% unless forloop.last %}, {% endunless %}{% endfor %} ],
+      "rarity" : "{{ post.item_rarity }}",
+      "attunement" : {{ post.item_attunement }},
+      "requirement" : "{{ post.item_requirement }}",
+      "curse" : {{ post.item_type }},
+      "classes" : [ {% for item in post.item_classes %} {"class" : "{{ item.class }}", "weight" : "{{ item.weight }}"}{% unless forloop.last %}, {% endunless %}{% endfor %} ],
+      "school" : "{{ post.item_school }}",
+      "role" : [ {% for item in post.item_role %} {"role" : "{{ item.role }}"}{% unless forloop.last %}, {% endunless %}{% endfor %} ],
+      "damage" : [ {% for item in post.item_damage %} {"type" : "{{ item.type }}"}{% unless forloop.last %}, {% endunless %}{% endfor %} ],
+      "tags" : [ {% for item in post.item_tags %} {"tag" : "{{ item.tag }}"}{% unless forloop.last %}, {% endunless %}{% endfor %} ],
+      "idea" : "{{ post.item_idea }}",
+      "co_creator" : "{{ post.item_co_creator }}",
+      "updated" : "{{ post.item_updated }}",
+      "content" : {{ post.content | smartify | jsonify }}
+    }{% unless forloop.last %},{% endunless %}
+  {% endfor %}
+]
