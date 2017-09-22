60861880e981f499029219aa94530f42aee3a24b
BornToDoStuff <borntodostuff@gmail.com>
2017-09-22 18:27:24 -0400

    mustache template test

--- _includes/js/custom-elements.js
+++ _includes/js/custom-elements.js
@@ -1,14 +1,6 @@
-class SpellElement extends HTMLElement {
-	constructor() {
-		// Always call super first in constructor
-		super();
-	}
-}
-customElements.define("magic-spell", SpellElement);
-if (customElements.get("magic-spell"))
-	console.log("magic-spell tag added");
-//HTMLAnchorElement
 
+class SpellElement extends HTMLElement { constructor() { super(); } }
+customElements.define("magic-spell", SpellElement);
 class MagicItemElement extends HTMLElement { constructor() { super(); } }
 customElements.define("magic-item", MagicItemElement);
 class ItemNameElement extends HTMLElement { constructor() { super(); } }
@@ -17,20 +9,3 @@ class ItemTagsElement extends HTMLElement { constructor() { super(); } }
 customElements.define("item-tags", ItemTagsElement);
 class ItemDescriptionElement extends HTMLElement { constructor() { super(); } }
 customElements.define("item-description", ItemDescriptionElement);
-
-var QuillIsDeclared = true;
-try{ Quill; }
-catch(e) {
-	if(e.name == "ReferenceError") {
-		QuillIsDeclared = false;
-	}
-}
-
-if (QuillIsDeclared)
-{
-	let Inline = Quill.import('blots/inline');
-	class SpellBlot extends Inline { }
-	SpellBlot.blotName = 'spell';
-	SpellBlot.tagName = 'magic-spell';
-	Quill.register(SpellBlot); //register custom tag as an item Quill can use
-}
