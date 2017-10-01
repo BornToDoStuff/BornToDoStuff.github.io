db9a91847fa9dd92f4dca191dbaac8a894269ddd
BornToDoStuff <borntodostuff@gmail.com>
2017-09-30 20:40:54 -0400

    adding more item details

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,10 @@
 /* lunr.min.js */
 /* ==================== */
 {% include /js/lunr.min.js %}
+
+{% comment %}
 /* ==================== */
 /* custom-elements.js */
 /* ==================== */
 {% include /js/custom-elements.js %}
+{% endcomment %}
