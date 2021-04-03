f3afc01ff9e860d6ea4b477df65641ddf0e0f30e
BornToDoStuff <borntodostuff@gmail.com>
2021-04-02 23:41:33 -0400

    try and fix show-more and numbering for related items

--- _includes/js/masonry-item-height.js
+++ _includes/js/masonry-item-height.js
@@ -1,20 +1,24 @@
-function setItemHeight(ele) {
+function setItemHeight(ele, resize = true) {
   var description = $(ele).find("item-description")[0];
   var heightCount = 0;
   var heightsIndex = heights.length - 1;
 
-  if ($(window).width() > 768) { //if its mobile just stick to 0 height
-    while (heightCount < heightsIndex &&
-      description.scrollHeight > sizes[heightCount].scrollHeight + 15 &&
-      description.scrollHeight > sizes[Math.min(heightsIndex,heightCount+1)].scrollHeight - 125)
-    { heightCount++; }
+  if (resize)
+  {
+    if ($(window).width() > 768) { //if its mobile just stick to 0 height
+      while (heightCount < heightsIndex &&
+        description.scrollHeight > sizes[heightCount].scrollHeight + 15 &&
+        description.scrollHeight > sizes[Math.min(heightsIndex,heightCount+1)].scrollHeight - 125)
+      { heightCount++; }
+    }
+
+    if ($(ele).hasClass("image")) { //except for if it has an image
+      heightCount = Math.max(1, heightCount); //it has to be at least "tall"
+    }
+
+    $(ele).addClass(heights[heightCount]);
   }
 
-  if ($(ele).hasClass("image")) { //except for if it has an image
-    heightCount = Math.max(1, heightCount); //it has to be at least "tall"
-  }
-
-  $(ele).addClass(heights[heightCount]);
   if (description.scrollHeight > description.clientHeight)
     $(ele).addClass("show-more");
 }
